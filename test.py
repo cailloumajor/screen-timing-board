23b31cdac3a769ffbf0e12eb071e450a61b84586
Arnaud Rocher <arnaud.roche3@gmail.com>
2019-05-28 17:11:16 +0200

    Start programming

---
+++ test.py
@@ -0,0 +1,78 @@
+import tkinter as tk
+import tkinter.font as tkfont
+
+from PIL import Image, ImageTk
+
+
+class FontAdjustingLabel(tk.Label):
+
+    def __init__(self, ht: int, master: tk.Widget, **kwargs: str) -> None:
+        super().__init__(master=master, **kwargs)
+        self.font = tkfont.Font(
+            family="Liberation Sans",
+            weight="bold",
+            size=-(ht // 2)
+        )
+        self.configure(font=self.font)
+        while self.winfo_reqheight() < ht:  # type: ignore
+            self.font["size"] = self.font["size"] - 1
+            self.update_idletasks()  # type: ignore
+
+
+class App(tk.Frame):
+    def __init__(self, master: tk.Tk) -> None:
+        super().__init__(master)
+        # self.configure(bg="black")
+        self.grid(sticky="nsew")
+
+        screen_height = self.winfo_screenheight()  # type: ignore
+        screen_width = self.winfo_screenwidth()  # type: ignore
+
+        logo_height = int(screen_height / 3)
+        logo = Image.open("logo.png")
+        logo.thumbnail((screen_width, logo_height))
+        logo_tk = ImageTk.PhotoImage(logo)
+        self.logo = tk.Label(self, image=logo_tk, bg="black")
+        # Keep a reference to avoid garbage collection
+        self.logo.image = logo_tk  # type: ignore
+
+        self.instruction = FontAdjustingLabel(
+            logo_height,
+            self,
+            text="BOX",
+            bg="black",
+            fg="dark violet"
+        )
+
+        timing_height = screen_height - logo_height
+        self.timing = FontAdjustingLabel(
+            timing_height,
+            self,
+            text="00.0",
+            bg="black",
+            fg="yellow"
+        )
+
+        self.logo.grid(row=0, column=0, sticky="nw")
+        self.instruction.grid(row=0, column=1, sticky="nsew")
+        self.timing.grid(row=1, columnspan=2, sticky="nsew")
+
+        self.columnconfigure(1, weight=1)
+
+        self.flash()
+
+    def flash(self) -> None:
+        bg = self.instruction["bg"]
+        fg = self.instruction["fg"]
+        if self.instruction["text"] or bg != "black":
+            self.instruction.configure(bg=fg, fg=bg)
+        self.after(500, self.flash)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.attributes("-fullscreen", 1)
+    board = App(root)
+    board.mainloop()
